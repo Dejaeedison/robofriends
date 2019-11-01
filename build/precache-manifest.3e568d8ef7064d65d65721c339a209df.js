5c3b7f61bf0b00ad9e918422d5db46443dc18c56
Dejae Edison <dejaeedison16@gmail.com>
2019-11-01 16:56:00 -0700

    adding the project

---
+++ build/precache-manifest.3e568d8ef7064d65d65721c339a209df.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bbd5baa031620c712dd49f5d19a9d967",
+    "url": "/index.html"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "e4e232d3ce43bc6fa359",
+    "url": "/static/css/main.319ee472.chunk.css"
+  },
+  {
+    "revision": "79c6f1758e88c906dcb6",
+    "url": "/static/js/2.80c069d3.chunk.js"
+  },
+  {
+    "revision": "e4e232d3ce43bc6fa359",
+    "url": "/static/js/main.b72c4a04.chunk.js"
+  },
+  {
+    "revision": "31175d55b7ad255ef221",
+    "url": "/static/js/runtime~main.6237261c.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/static/media/SEGA.12a3db8b.woff"
+  }
+]);
