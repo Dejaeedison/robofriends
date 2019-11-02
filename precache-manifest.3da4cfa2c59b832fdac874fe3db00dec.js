ac0ae7cb87d262967fb5e6d39ffa4ed56da96bd6
Dejae Edison <dejaeedison16@gmail.com>
2019-11-01 17:55:40 -0700

    Updates

---
+++ precache-manifest.3da4cfa2c59b832fdac874fe3db00dec.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee0e15b23ac5a97b830ecdb9981df1c1",
+    "url": "/robofriends/index.html"
+  },
+  {
+    "revision": "3f93da37f8c53b50a6d6",
+    "url": "/robofriends/static/css/2.31dafeb2.chunk.css"
+  },
+  {
+    "revision": "0ee93b107568322019b9",
+    "url": "/robofriends/static/css/main.f1ab460d.chunk.css"
+  },
+  {
+    "revision": "3f93da37f8c53b50a6d6",
+    "url": "/robofriends/static/js/2.8fe3225a.chunk.js"
+  },
+  {
+    "revision": "0ee93b107568322019b9",
+    "url": "/robofriends/static/js/main.09b81123.chunk.js"
+  },
+  {
+    "revision": "8bd552311f5d528a5605",
+    "url": "/robofriends/static/js/runtime~main.e3d5a5e7.js"
+  },
+  {
+    "revision": "12a3db8ba7f5423327bb12aed3c41977",
+    "url": "/robofriends/static/media/SEGA.12a3db8b.woff"
+  }
+]);
